0a6026512eca428151c5e873c45b7c59a18cf1aa
remygist <remy.gistelinck@student.ehb.be>
2024-05-09 22:54:12 +0200

    Update dashboard.blade.php

--- code/course-project/resources/views/dashboard.blade.php
+++ code/course-project/resources/views/dashboard.blade.php
@@ -1,5 +1,13 @@
 <x-app-layout>
-<h1>You're logged in.</h1>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @foreach($movies as $movie)
+                <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{$movie->title}}
+                </h1>
+                <p>{{$movie->description}}</p>
 
-
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
