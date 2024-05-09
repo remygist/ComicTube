5efc1b19de068fe95ed994183d818bcb293342a2
remygist <remy.gistelinck@student.ehb.be>
2024-05-09 23:30:26 +0200

    added navlink or movies and shows

--- code/course-project/resources/views/layouts/navigation.blade.php
+++ code/course-project/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Movies') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Shows') }}
+                    </x-nav-link>
                 </div>
             </div>
 
