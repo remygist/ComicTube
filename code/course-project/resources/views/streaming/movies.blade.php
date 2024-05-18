25b9ef5f491d6f31dea1eca3e20cbede651c2e15
remygist <remy.gistelinck@student.ehb.be>
2024-05-18 17:07:19 +0200

    modified placeholder in searchbar

--- code/course-project/resources/views/streaming/movies.blade.php
+++ code/course-project/resources/views/streaming/movies.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <form action="{{route('search')}}">
-                <input type="text" name="query" placeholder="Search movie" value="{{request('query')}}">
+                <input type="text" name="query" placeholder="Search movie or show" value="{{request('query')}}">
                 <button type="submit" class="custom-button">
                     Search
                 </button>
