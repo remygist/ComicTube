0175da83458bcc8f2dbc5392e4a95f1e1370d322
remygist <remy.gistelinck@student.ehb.be>
2024-05-18 17:00:12 +0200

    go to register btn on login

--- code/course-project/resources/views/auth/login.blade.php
+++ code/course-project/resources/views/auth/login.blade.php
@@ -33,15 +33,25 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('No account yet?') }}
+            </a>
+
+            <div style="margin-right: 10px;"></div>
+
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
+            <div style="margin-right: 10px;"></div>
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+
     </form>
 </x-guest-layout>
